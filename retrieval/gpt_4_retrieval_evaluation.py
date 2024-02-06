9bff91465c35772c8156f428e96d57c1466787f7
Daniele Grandi <daniele.grandi@autodesk.com>
2024-02-06 11:42:02 -0800

    added code for evaluating retrieval and results

--- retrieval/gpt_4_retrieval_evaluation.py
+++ retrieval/gpt_4_retrieval_evaluation.py
@@ -1,50 +1,19 @@
+import re
+import string
+from collections import Counter
 from openai import OpenAI
-from config import open_ai_key
 from time import sleep
 from itertools import cycle
-
-# client = OpenAI(api_key=open_ai_key)
-
-# file_name =
-
-# # Upload a file with an "assistants" purpose
-# file = client.files.create(
-#     file=open(file_name, "rb"),
-#     purpose='assistants'
-# )
-
-# # Add the file to the assistant
-# assistant = client.beta.assistants.create(
-#     instructions="You are a customer support chatbot. Use your knowledge base to best respond to customer queries.",
-#     model="gpt-4-1106-preview",
-#     tools=[{"type": "retrieval"}],
-#     file_ids=[file.id]
-# )
-
-# message = client.beta.threads.messages.create(
-#     thread_id=thread.id,
-#     role="user",
-#     content="I can not find in the PDF manual how to turn off this device.",
-#     file_ids=[file.id]
-# )
-
-# # # Function to ask chat gpt a question and get the result
-# # def ask_chat_gpt(question):
-# #     completion = client.chat.completions.create(
-# #     model="gpt-4-1106-preview",
-# #     messages=[
-# #         {"role": "system", "content": "You are an expert on NASA technical standards, and you are helping to create exam questions that test engineers' knowledge of the standards."},
-# #         {"role": "user", "content": question}
-# #     ]
-# #     )
-# #     return completion.choices[0].message.content
+import os
+import pandas as pd
+from tqdm import tqdm
 
 
 def upload_file(file_path):
     # Upload a file with an "assistants" purpose
     upload_file = client.files.create(
-    file=open(file_path, "rb"),
-    purpose='assistants')
+        file=open(file_path, "rb"),
+        purpose='assistants')
 
     return upload_file
 
@@ -68,53 +37,124 @@ def run_thread(question):
         content=question)
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
-        assistant_id=assistant.id,)
+        assistant_id=assistant.id, )
     status_complete = False
-    for i in cycle(["|", "/", "-", "\\"]):
-        print(f"Loading... {i}",end='\r') # '\r' clears the previous output
+    i = 0
+    while status_complete is False and i < 0.5*1000:
         run = client.beta.threads.runs.retrieve(
             thread_id=thread.id,
             run_id=run.id)
         status_complete = run.status == "completed"
-        if status_complete:
-            break
         sleep(0.5)
+        i += 1
 
         # Retrieve the message object
         messages = client.beta.threads.messages.list(
             thread_id=thread.id)
 
-    # Extract the message content
-    message_content = messages.data[0].content[0].text
-    annotations = message_content.annotations
-    citations = []
+    return messages.data[0].content[0].text.value
 
-    # Iterate over the annotations and add footnotes
-    for index, annotation in enumerate(annotations):
-        # Replace the text with a footnote
-        message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
 
-        # Gather citations based on annotation attributes
-        if (file_citation := getattr(annotation, 'file_citation', None)):
-            cited_file = client.files.retrieve(file_citation.file_id)
-            citations.append(f'[{index}] {file_citation.quote} from {cited_file.filename}')
-        elif (file_path := getattr(annotation, 'file_path', None)):
-            cited_file = client.files.retrieve(file_path.file_id)
-            citations.append(f'[{index}] Click <here> to download {cited_file.filename}')
-            # Note: File download functionality not implemented above for brevity
+def normalize_answer(s):
+    """
+    Taken from the official evaluation script for v1.1 of the SQuAD dataset.
+    Lower text and remove punctuation, articles and extra whitespace.
+    """
 
-    # Add footnotes to the end of the message before displaying to user
-    message_content.value += '\n\n' + '\n'.join(citations)
+    def remove_articles(text):
+        return re.sub(r"\b(a|an|the)\b", " ", text)
 
-    return message_content.value
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def token_f1_score(prediction, ground_truth):
+    # if prediction is None or pd.isnull(prediction):
+    #     prediction = ""
+    # elif ground_truth is None or pd.isnull(ground_truth):
+    #     ground_truth = ""
+
+
+    """
+    Taken from the official evaluation script for v1.1 of the SQuAD dataset.
+    """
+    prediction_tokens = normalize_answer(prediction).split()
+    ground_truth_tokens = normalize_answer(ground_truth).split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+
+def combine_prediction(row):
+    rule_number = row['rule_number']
+    rule_text = row['rule_text']
+    rule_title = row['rule_title']
+
+    if rule_number is None or pd.isnull(rule_number):
+        rule_number = ""
+    if rule_text is None or pd.isnull(rule_text):
+        rule_text = ""
+    if rule_title is None or pd.isnull(rule_title):
+        rule_title = ""
+
+    return ' '.join([rule_number, rule_title, rule_text])
+
 
 if __name__ == '__main__':
-    client = OpenAI(api_key=open_ai_key)
-    file_path = 'docs/FSAE_Rules_2024_V1 (2).pdf'
-    file = upload_file(file_path)
-    assistant = create_assistant(file)
+    # get the api key from memory
+    from config import OPENAI_API_KEY
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
-    question = "We are a student engineering team designing a vehicle for the FSAE competition. Attached is the FSAE rules document. List all the rules that relate to the suspension. Answer only with the rules numbers."
+    # file_path = 'docs/FSAE_Rules_2024_V1 (2).pdf'
+    # file = upload_file(file_path)
+    # assistant = create_assistant(file)
 
-    response = run_thread(question)
-    print(response)
+    questions_pd = pd.read_csv("docs/FSAE DesignSpecQA Benchmark - retrieval.csv")
+    for index, row in tqdm(questions_pd.iterrows(), desc='generating responses', total=len(questions_pd)):
+        # if response column is not empty, skip the row
+        try:
+            response = row['response']
+        except KeyError:
+            response = None
+        if not pd.isnull(response):
+            continue
+
+        rule_number = row['rule_number']
+        rule_text = row['rule_text']
+
+        # Exclude the following questions
+        if rule_text is None or pd.isnull(rule_text) or rule_number.split('.')[0] in ['GR', 'AR', 'DR']:
+            continue
+
+        # Compile the question
+        question = f"We are a student engineering team designing a vehicle for the FSAE competition. Attached is the " \
+                   f"FSAE rules document. What does rule {rule_number} state exactly? Answer with only the text of " \
+                   f"the rule and no other words."
+
+        # Run through model
+        response = run_thread(question)
+
+        # Save the response
+        questions_pd.loc[index, 'question'] = question
+        questions_pd.loc[index, 'response'] = response.split("【")[0] + "."
+        questions_pd.to_csv("docs/FSAE DesignSpecQA Benchmark - retrieval.csv", index=False)
+
+    # compute the f1 score between the answer and the response
+    questions_pd['f1 score'] = questions_pd[questions_pd[['rule_text', 'response']].notnull().all(1)].apply(lambda row: token_f1_score(combine_prediction(row), row['response']), axis=1)
+
+    # save the results
+    questions_pd.to_csv("docs/FSAE DesignSpecQA Benchmark - retrieval.csv", index=False)
