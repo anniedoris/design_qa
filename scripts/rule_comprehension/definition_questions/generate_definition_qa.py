73f85f9708b15239963cf94ee8c6eab09d1b1c08
annie_doris <annie@culturerobotics.com>
2024-02-12 10:59:33 -0500

    fixing issue with image number missing

--- scripts/rule_comprehension/definition_questions/generate_definition_qa.py
+++ scripts/rule_comprehension/definition_questions/generate_definition_qa.py
@@ -6,6 +6,7 @@ from common_prompts import prompt_preamble
 
 if __name__ == '__main__':
     qa = []
+    current_image_num = 1
     question = prompt_preamble + "Also attached is an image showing six CAD views of our vehicle design." \
         " According to the rules, your analysis of the image, and any other knowledge, what is the name " \
             "of the component(s) highlighted in pink? Answer just with the name of the highlighted component(s) and nothing else."
@@ -15,11 +16,13 @@ if __name__ == '__main__':
             "highlighted component(s) can better be visualized. Answer just with the name of the highlighted component(s) and nothing else."
     df = pd.read_csv('definitions.csv')
     for i, row in df.iterrows():
-        if row['hidden_components'] == "yes":
-            qa.append([question_hidden, row['highlighted_component']])
-        else:
-            qa.append([question, row['highlighted_component']])
         
-    pd.DataFrame(qa, columns=['question', 'answer']).to_csv("../../../dataset/rule_comprehension/rule_definition_qa.csv", index=False)
+        if row['hidden_components'] == "yes":
+            qa.append([question_hidden, row['highlighted_component'], str(current_image_num) + '.jpg'])
+        else:
+            qa.append([question, row['highlighted_component'], str(current_image_num) + '.jpg'])
+        current_image_num += 1
+        
+    pd.DataFrame(qa, columns=['question', 'answer', 'image']).to_csv("../../../dataset/rule_comprehension/rule_definition_qa.csv", index=False)
     
     
