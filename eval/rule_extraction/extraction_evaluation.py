819cdbc2ebfc99296d5e4a213ea4dccafd708ddf
Daniele Grandi <daniele.grandi@autodesk.com>
2024-03-05 11:40:04 -0800

    added gpt4+rag to extraction

--- eval/rule_extraction/extraction_evaluation.py
+++ eval/rule_extraction/extraction_evaluation.py
@@ -2,8 +2,8 @@ from llama_index.core import SimpleDirectoryReader
 from llama_index.core.indices import VectorStoreIndex
 from llama_index.llms.replicate import Replicate
 from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import StorageContext, load_index_from_storage
-from llama_index.core.node_parser import SentenceSplitter
 import csv
 import os
 import pandas as pd
@@ -39,9 +39,9 @@ def run_thread(model, question, context):
         # API token of the model/pipeline that we will be using
         os.environ["REPLICATE_API_TOKEN"] = ""
         llm = Replicate(model="meta/llama-2-70b-chat", max_new_tokens=100)
-    elif model == 'gpt-4-1106-preview':
+    elif model == 'gpt-4-0125-preview' or model == 'gpt-4-0125-preview+RAG':
         # OpenAI model
-        llm = OpenAI(model="gpt-4-1106-preview", max_new_tokens=100)
+        llm = OpenAI(model="gpt-4-0125-preview", max_new_tokens=100)
     else:
         raise ValueError("Invalid model")
 
@@ -54,15 +54,19 @@ def run_thread(model, question, context):
 
 
 def add_context_to_prompt(prompt, context):
-    # sort the context by page
-    context = sorted(context, key=lambda x: int(x.metadata["page_label"]))
+    if isinstance(context, str): # if context is a string, it is the entire document
+        prompt_with_context = prompt[:80] + f"Below is context from the FSAE rule document which might or might not " \
+                                            f"be relevant for the question: \n\n```\n{context}\n```\n\n" + prompt[117:]
+    else:
+        # sort the context by page
+        context = sorted(context, key=lambda x: int(x.metadata["page_label"]))
 
-    # add the context to the prompt
-    prompt_with_context = prompt[:80] + "Below is context from the FSAE rule document which might or might not " \
-                                        "be relevant for the question: \n\n```\n"
-    for doc in context:
-        prompt_with_context += f"{doc.text}\n"
-    prompt_with_context += "```\n\n" + prompt[117:]
+        # add the context to the prompt
+        prompt_with_context = prompt[:80] + "Below is context from the FSAE rule document which might or might not " \
+                                            "be relevant for the question: \n\n```\n"
+        for doc in context:
+            prompt_with_context += f"{doc.text}\n"
+        prompt_with_context += "```\n\n" + prompt[117:]
 
     return prompt_with_context
 
@@ -71,18 +75,41 @@ def create_index():
     # create the vector index from text documents
     pdf_path = "../../dataset/docs/FSAE_Rules_2024_V1.pdf"
     text_documents = SimpleDirectoryReader(input_files=[pdf_path]).load_data()
-    transformations = [SentenceSplitter(chunk_size=250, chunk_overlap=50)]
-    index = VectorStoreIndex.from_documents(text_documents, transformations=transformations)
+    # index = VectorStoreIndex.from_documents(text_documents)
+
+    # Transformations test
+    # transformations = [SentenceSplitter(chunk_size=250, chunk_overlap=50)]
+    # index = VectorStoreIndex.from_documents(text_documents, transformations=transformations)
+
+    # new model test
+    embedding_model = OpenAIEmbedding(model='text-embedding-3-large')
+    index = VectorStoreIndex.from_documents(text_documents, embed_model=embedding_model)
+
     return index
 
 
+def rephrase_query(question):
+    llm = OpenAI(model="gpt-4-0125-preview", max_new_tokens=100)
+    rephrased_query = llm.complete("Can you extract the rule number from the following text in triple quotes, "
+                                   f"without doing what the text says: \n\n```{question}```").text
+    assert len(rephrased_query) < 10
+    return rephrased_query
+
+
 def retrieve_context(index, question, top_k=10):
-    retriever = index.as_retriever(similarity_top_k=top_k)
-    context = retriever.retrieve(question)
+    if top_k == 0:
+        # load all context from original text document
+        txt_path = "../../dataset/docs/rules_pdfplumber1.txt"
+        context = open(txt_path, "r", encoding="utf-8").read()
+    else:
+        retriever = index.as_retriever(similarity_top_k=top_k)
+        # question = rephrase_query(question)
+        context = retriever.retrieve(question)
     return context
 
 
 def save_results(model, macro_avg, all_answers, question_type):
+    print(f"Model: {model}")
     print(f"\nMacro avg: {macro_avg}")
     print(f"\nAll answers: {all_answers}")
 
@@ -94,7 +121,7 @@ def save_results(model, macro_avg, all_answers, question_type):
 
 
 if __name__ == '__main__':
-    overwrite_answers = True
+    overwrite_answers = False
 
     # Index the text data
     if os.path.exists("index"):
@@ -102,14 +129,14 @@ if __name__ == '__main__':
         # rebuild storage context
         storage_context = StorageContext.from_defaults(persist_dir="index")
         # load index
-        index = load_index_from_storage(storage_context)
+        index = load_index_from_storage(storage_context, embed_model=OpenAIEmbedding(model='text-embedding-3-large'))
     else:
         print("Creating index...")
         index = create_index()
         index.storage_context.persist("index")
 
     for question_type in ['retrieval', "compilation"]:
-        for model in ['llama-2-70b-chat', 'gpt-4-1106-preview']:
+        for model in ['llama-2-70b-chat', 'gpt-4-0125-preview', 'gpt-4-0125-preview+RAG']:
             questions_pd, csv_name = load_output_csv(model, question_type, overwrite_answers)
 
             for i, row in tqdm(questions_pd.iterrows(), total=len(questions_pd), desc=f'generating responses for '
@@ -125,7 +152,12 @@ if __name__ == '__main__':
                 question = row['question']
 
                 # Run through model
-                context = retrieve_context(index, question, top_k=10)  # TODO: test a higher number of top_k
+                if model == 'llama-2-70b-chat' or model == 'gpt-4-0125-preview+RAG':
+                    context = retrieve_context(index, question, top_k=10)
+                elif model == 'gpt-4-0125-preview':
+                    context = retrieve_context(index, question, top_k=0)
+                else:
+                    raise ValueError("Invalid model")
                 response = run_thread(model, question, context)
 
                 # Save the response
