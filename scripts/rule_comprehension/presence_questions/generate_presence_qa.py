61022ed94f7eed7359efe158e13186e31e21b25c
annie_doris <annie@culturerobotics.com>
2024-03-06 02:10:07 -0500

    presence dataset finalized

--- scripts/rule_comprehension/presence_questions/generate_presence_qa.py
+++ scripts/rule_comprehension/presence_questions/generate_presence_qa.py
@@ -7,18 +7,6 @@ from common_prompts import prompt_preamble
 if __name__ == '__main__':
     qa = []
     current_image_num = 1
-    # common_question1 = "Also attached is an image showing six CAD views of our vehicle design." \
-    #     " What is the name of the component(s) highlighted in pink?"
-    # common_question2 = " Answer just with the name of the highlighted component(s) and nothing else."
-    # question = prompt_preamble + common_question1 + common_question2
-    # question_hidden = prompt_preamble + common_question1 + " Some parts of the design have been hidden so that the " \
-    #         "highlighted component(s) can better be visualized." + common_question2
-            
-            
-    # question = prompt_preamble + "Also attached is an image showing seven CAD views (each boxed in black) of our vehicle design. The top, big view " \
-    #     "shows a close-up view of the design. The six smaller views on the bottom of the image show different complete views of the CAD  " \
-    #     "of the vehicle and are provided for context. Note that the close-up view orientation matches one of the six complete view orientations. Looking at the close-up view, is the " \
-    #     + XX + " visible in the close-up view? Answer simply with yes or no."
         
     df = pd.read_csv('presence_raw.csv')
     for i, row in df.iterrows():
@@ -31,10 +19,10 @@ if __name__ == '__main__':
             present_components = "the " + all_ground_truth_components[0]
         
         question = prompt_preamble + "Also attached is an image showing seven CAD views (each boxed in black) of our vehicle design. The top, big view " \
-        "shows a close-up view of the design. The six smaller views on the bottom of the image show different complete views of the CAD  " \
+        "shows a close-up view of the design. The six smaller views on the bottom of the image show different complete views of the CAD " \
         "of the vehicle and are provided for context. Note that the close-up view orientation matches one of the six complete view orientations. " \
         "The close-up view may also have some components hidden (with respect to the corresponding complete view) for visualization of specific components. " \
-        "Looking at the close-up view, is/are " + present_components + " visible in the close-up view? Answer simply with yes or no."
+        "Looking at the close-up view, is/are any part of the " + present_components + " visible in the close-up view? Answer simply with yes or no."
         
         # TODO: add mentions in rules back like definition_qa
         qa.append([question, row['present'], str(current_image_num) + '.jpg', row['mention_in_rules']])
