48666c42f7ff5f79932b30fbbf663ca0e598b83e
anniedoris <anniecd88@gmail.com>
2024-08-22 23:55:26 -0400

    adding similarity scores for func performances qs

--- eval/rule_compliance/just_score.py
+++ eval/rule_compliance/just_score.py
@@ -1,10 +1,40 @@
-# Compute the accuracy of the responses
+
 import sys
 sys.path.append('../metrics')
+from metrics import eval_functional_performance_qa
 from metrics import eval_dimensions_qa
 
-def save_results(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus,
-                         all_bleus, macro_avg_rogues, all_rogues):
+csv_name = 'dimension_functional_performance_evaluation_llava-13b+idealRAG5.csv'
+# Options are functional_performance, detailed_context, or context
+question_type = 'functional_performance'
+model = 'llava-13b+idealRAG5'
+
+def save_results_functional_performance(model, macro_avg_accuracy, all_accuracies, macro_avg_bleus, all_bleus, macro_avg_rogues, all_rogues, macro_similarity, all_similarities):
+    print(f"Model: {model}")
+    print(f"\nMacro avg: {macro_avg_accuracy}")
+    print(f"\nAll accuracies: {all_accuracies}")
+    print(f"\nMacro avg bleus: {macro_avg_bleus}")
+    print(f"\nAll bleus: {all_bleus}")
+    print(f"\nMacro avg rogues: {macro_avg_rogues}")
+    print(f"\nAll rogues: {all_rogues}")
+    print(f"\nMacro avg similarities: {macro_similarity}")
+    print(f"\nAll rogues: {all_similarities}")
+
+    # Save results to txt file
+    with open(f"dimension_{question_type}_evaluation_{model}.txt", "w") as text_file:
+        text_file.write(f"Model: {model}")
+        text_file.write(f"\nMacro avg: {macro_avg_accuracy}")
+        text_file.write(f"\nAll accuracies: {all_accuracies}")
+        text_file.write(f"\nMacro avg bleus: {macro_avg_bleus}")
+        text_file.write(f"\nAll bleus: {all_bleus}")
+        text_file.write(f"\nMacro avg rogues: {macro_avg_rogues}")
+        text_file.write(f"\nAll rogues: {all_rogues}")
+        text_file.write(f"\nMacro similarities: {macro_similarity}")
+        text_file.write(f"\nAll similarities: {all_similarities}")
+    return
+
+def save_results_dimension(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus,
+                         all_bleus, macro_avg_rogues, all_rogues, macro_similarity, all_similarities):
     print(f"Model: {model}")
     print(f"\nMacro avg: {macro_avg_accuracy}")
     print(f"\nDirect Dimension avg: {direct_dim_avg}")
@@ -14,9 +44,11 @@ def save_results(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_a
     print(f"\nAll bleus: {all_bleus}")
     print(f"\nMacro avg rogues: {macro_avg_rogues}")
     print(f"\nAll rogues: {all_rogues}")
+    print(f"\nMacro avg similarities: {macro_similarity}")
+    print(f"\nAll rogues: {all_similarities}")
 
     # Save results to txt file
-    with open(f"dimension_{question_type}_evaluation_{model}.txt", "w") as text_file:
+    with open(f"dimension_{question_type}_evaluation_{model}_test.txt", "w") as text_file:
         text_file.write(f"Model: {model}")
         text_file.write(f"\nMacro avg: {macro_avg_accuracy}")
         text_file.write(f"\nDirect Dimension avg: {direct_dim_avg}")
@@ -26,18 +58,21 @@ def save_results(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_a
         text_file.write(f"\nAll bleus: {all_bleus}")
         text_file.write(f"\nMacro avg rogues: {macro_avg_rogues}")
         text_file.write(f"\nAll rogues: {all_rogues}")
+        text_file.write(f"\nMacro similarities: {macro_similarity}")
+        text_file.write(f"\nAll similarities: {all_similarities}")
+    return
 
-model = 'llava-13b+idealRAG'
-question_type = 'context'
-rag_num = '2'
 
-csv_name = f"dimension_{question_type}_evaluation_{model}{rag_num}.csv"
-print(csv_name)
+if question_type == 'functional_performance':
+    # Compute the accuracy of the responses
+    macro_avg_accuracy, all_accuracies, macro_avg_bleus, all_bleus, macro_avg_rogues, all_rogues, macro_similarity, all_similarities = eval_functional_performance_qa(csv_name)
 
- # Compute the accuracy of the responses
-macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus, all_bleus, \
-    macro_avg_rogues, all_rogues = eval_dimensions_qa(csv_name)
+    # Print and save the results
+    save_results_functional_performance(model, macro_avg_accuracy, all_accuracies, macro_avg_bleus, all_bleus, macro_avg_rogues, all_rogues, macro_similarity, all_similarities)
 
-# Print and save the results
-save_results(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus,
-                all_bleus, macro_avg_rogues, all_rogues)
+else:
+    macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus, all_bleus, \
+                macro_avg_rogues, all_rogues, macro_similarity, all_similarities = eval_dimensions_qa(csv_name)
+    
+    save_results_dimension(model, macro_avg_accuracy, direct_dim_avg, scale_bar_avg, all_accuracies, macro_avg_bleus,
+                        all_bleus, macro_avg_rogues, all_rogues, macro_similarity, all_similarities)
